24f32d9322d915613c1c7a94de28f017b296e1e5
Yunus Emre Geldegül <yunusemregeldegul@gmail.com>
2021-02-13 15:06:36 +0300

    delete all files

--- app/routes/main.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, render_template
-
-main = Blueprint('main', __name__, url_prefix='/')
-
-@main.route('/')
-@main.route('/index')
-def index():
-    return render_template('views/main/index.html', title="Main")
