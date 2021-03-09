7afe38529a9249ba701fef666a567d373a1e53df
Yunus Emre Geldegül <yunusemregeldegul@gmail.com>
2021-03-09 09:29:14 +0300

    add error handle

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from os import path
 
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
@@ -8,6 +8,14 @@ from flask_login import LoginManager
 db = SQLAlchemy()
 migrate = Migrate()
 login_manager = LoginManager()
+
+
+def page_not_found(e):
+    return render_template('assets/404.html'), 404
+
+
+def international_server_error(e):
+    return render_template('assets/500.html'), 500
 
 
 def create_app():
@@ -21,6 +29,9 @@ def create_app():
     login_manager.login_view = 'auth.login'
     login_manager.login_message = 'Please Login To Access This Page'
     login_manager.login_message_category = 'info'
+
+    app.register_error_handler(404, page_not_found)
+    app.register_error_handler(500, international_server_error)
 
     from app.routes.main import main
     app.register_blueprint(main)
