381c83ac740cc50aa1d814f4481dfd44204c5990
Yunus Emre Geldegül <yunusemregeldegul@gmail.com>
2021-04-07 20:11:14 +0300

    change settings file

--- settings.py
+++ settings.py
@@ -3,8 +3,4 @@ from os import path, urandom
 BASEDIR = path.abspath(path.dirname(__file__))
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + path.join(BASEDIR, 'demongo.db')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-try:
-    from settings_local import *
-except:
-    pass
+SECRET_KEY = "demongo"
